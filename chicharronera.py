0b42739a80e04f91b13c9926b7d8c2184acd4a03
JavierEstrada53 <79867491+JavierEstrada53@users.noreply.github.com>
2021-09-26 13:49:38 -0500

    Add files via upload
    Tarea 4. La chicharronera. Javier Figueroa Estrada EDD.

--- chicharronera.py
+++ chicharronera.py
@@ -3,15 +3,12 @@ Estructura de datos'''
 def la_chicharronera():
 
     a= float(input('Introduzca el valor de a : ')) # valor flotante
-    if a == 0:
-        print(' El valor de "a"  no puede ser 0')
+    while a == 0:
+        print(' Error, el valor de "a"  no puede ser 0')
         a= float(input('Vuelva a introducir el valor de a : '))
-        b= float(input('Introduzca el valor de b : '))
-        c= float(input('introduzca el valor de c : '))
-    else:
 
-        b= float(input('Introduzca el valor de b : '))
-        c= float(input('introduzca el valor de c : '))
+    b= float(input('Introduzca el valor de b : '))
+    c= float(input('introduzca el valor de c : '))
     
     #Definimos la chicharronera
     # para no llamar a otra librería, elevamos a la 0.5 = 1/2, es decir, una raíz cuadrada 
